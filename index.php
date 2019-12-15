e47dca773b75845701194514892bf7de18482ee0
Indra Gunawan <55778242+admingunawan@users.noreply.github.com>
2019-12-15 18:07:08 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,807 @@
+<html><head>   
+	<meta name="Description" content="Silent's"> 
+	<link rel="shortcut icon" href="https://scontent-sin6-1.xx.fbcdn.net/v/t1.0-9/79455270_135258504569924_1779422465609433088_n.jpg?_nc_cat=110&amp;_nc_ohc=LsxQs-rmHuIAQlp1ZBPoK5MAbHRh9N7dYZZeO7XMqrq0ju8Q6OPsizl5A&amp;_nc_ht=scontent-sin6-1.xx&amp;oh=a86a7f71a5aba71b3f2703481f4fddaf&amp;oe=5E736198" "="" type="image/png"> 
+	<meta property="og:image" content="https://scontent-sin6-1.xx.fbcdn.net/v/t1.0-9/79455270_135258504569924_1779422465609433088_n.jpg?_nc_cat=110&amp;_nc_ohc=LsxQs-rmHuIAQlp1ZBPoK5MAbHRh9N7dYZZeO7XMqrq0ju8Q6OPsizl5A&amp;_nc_ht=scontent-sin6-1.xx&amp;oh=a86a7f71a5aba71b3f2703481f4fddaf&amp;oe=5E736198"> 
+	<link href="http://fonts.googleapis.com/css?family=Arvo|Electrolize|Iceberg" rel="stylesheet" type="text/css"> 
+	<link href="http://fonts.googleapis.com/css?family=Nova+Square" rel="stylesheet" type="text/css">
+	<title> Hacked by Silent's Space</title> 
+  </head>
+  <body onload="play()" oncontextmenu="return false;" onkeydown="return false;" onmousedown="return false;">
+	<audio id="song" source="mp3" src="http://m1705.wapka-file.com//g03/music/29574416/3475468/c249YZha8ox^ij3e5zQP1SXJfS4SI9sBquyLP2IP1u1b08mlxQ/7e7c3.mp3" autoplay="autoplay" loop="loop"></audio>
+<style>
+    
+@import url(http://fonts.googleapis.com/css?family=Gilda+Display);
+html { 
+  background:#000;font-family:Courier new;text-align:center;color:#fff
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+.error {
+  text-align: center;
+  font-family: 'Gilda Display', serif;
+  
+  text-align: center;
+  width: 100%;
+  height: 120px;
+  margin: auto;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: -60px;
+  right: 0;
+  -webkit-animation: noise-3 1s linear infinite;
+          animation: noise-3 1s linear infinite;
+  overflow: default;
+}
+body:after {
+  content: 'Silent's Space';
+  font-family: OCR-A;
+  font-size: 100px;
+  
+  text-align: center;
+  width: 550px;
+  margin: auto;
+  position: absolute;
+  top: 25%;
+  bottom: 0;
+  left: 0;
+  right: 35%;
+  opacity: 0;
+  color: white;
+  -webkit-animation: noise-1 .2s linear infinite;
+          animation: noise-1 .2s linear infinite;
+}
+body:before {
+  content: 'Silent's Space';
+  font-family: OCR-A;
+  font-size: 100px;
+  
+  text-align: center;
+  width: 550px;
+  margin: auto;
+  position: absolute;
+  top: 25%;
+  bottom: 0;
+  left: 0;
+  right: 35%;
+  opacity: 0;
+  color: white;
+  -webkit-animation: noise-2 .2s linear infinite;
+          animation: noise-2 .2s linear infinite;
+}
+.error {
+  text-align: center;
+  width: 200px;
+  height: 60px;
+  margin: auto;
+  position: absolute;
+  top: 280px;
+  bottom: 0;
+  left: 20px;
+  right: 0;
+  -webkit-animation: noise-3 1s linear infinite;
+          animation: noise-3 1s linear infinite;
+}
+.info:before {
+  content: 'Silent's Space';
+  font-family: OCR-A;
+  font-size: 100px;
+  text-align: center;
+  width: 800px; 
+  margin: auto;
+  position: absolute;
+  top: 20px;
+  bottom: 0;
+  left: 40px;
+  right: 100px;
+  opacity: 0;
+  color: white;
+  -webkit-animation: noise-2 .2s linear infinite;
+          animation: noise-2 .2s linear infinite;
+}
+.info:after {
+  content: 'Silent's Space';
+  font-family: OCR-A;
+  font-size: 100px;
+  text-align: center;
+  width: 800px;
+  margin: auto;
+  position: absolute;
+  top: 20px;
+  bottom: 0;
+  left: 40px;
+  right: 0;
+  opacity: 0;
+  color: white;
+  -webkit-animation: noise-1 .2s linear infinite;
+          animation: noise-1 .2s linear infinite;
+}
+@-webkit-keyframes noise-1 {
+  0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;}
+  10% {opacity: .1;}
+  50% {opacity: .5; left: -6px;}
+  80% {opacity: .3;}
+  100% {opacity: .6; left: 2px;}
+}
+@keyframes noise-1 {
+  0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;}
+  10% {opacity: .1;}
+  50% {opacity: .5; left: -6px;}
+  80% {opacity: .3;}
+  100% {opacity: .6; left: 2px;}
+}
+@-webkit-keyframes noise-2 {
+  0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;}
+  10% {opacity: .1;}
+  50% {opacity: .5; left: 6px;}
+  80% {opacity: .3;}
+  100% {opacity: .6; left: -2px;}
+}
+@keyframes noise-2 {
+  0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;}
+  10% {opacity: .1;}
+  50% {opacity: .5; left: 6px;}
+  80% {opacity: .3;}
+  100% {opacity: .6; left: -2px;}
+}
+@-webkit-keyframes noise {
+  0%, 3%, 5%, 42%, 44%, 100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);}  
+  4.3% {opacity: 1; -webkit-transform: scaleY(1.7); transform: scaleY(1.7);}
+  43% {opacity: 1; -webkit-transform: scaleX(1.5); transform: scaleX(1.5);}
+}
+@keyframes noise {
+  0%, 3%, 5%, 42%, 44%, 100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);}  
+  4.3% {opacity: 1; -webkit-transform: scaleY(1.7); transform: scaleY(1.7);}
+  43% {opacity: 1; -webkit-transform: scaleX(1.5); transform: scaleX(1.5);}
+}
+@-webkit-keyframes noise-3 {
+  0%,3%,5%,42%,44%,100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);}
+  4.3% {opacity: 1; -webkit-transform: scaleY(4); transform: scaleY(4);}
+  43% {opacity: 1; -webkit-transform: scaleX(10) rotate(60deg); transform: scaleX(10) rotate(60deg);}
+}
+@keyframes noise-3 {
+  0%,3%,5%,42%,44%,100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);}
+  4.3% {opacity: 1; -webkit-transform: scaleY(4); transform: scaleY(4);}
+  43% {opacity: 1; -webkit-transform: scaleX(10) rotate(60deg); transform: scaleX(10) rotate(60deg);}
+}
+.wrap {
+  top: 30%;
+  left: 25%;
+  
+  height: 200px;
+  
+  margin-top: -100px;
+  position: absolute;
+}
+code {
+  color: white;
+}
+span.blue {
+  color: #48beef;
+}
+span.comment {
+  color: #7f8c8d;
+}
+span.orange {
+  color: #f39c12;
+}
+span.green {
+  color: #33cc33;
+}
+.viewFull {
+  font-family:OCR-A;
+  color:orange;
+  text-decoration:;
+}
+    
+}
+ @media only screen and (min-height: 500px) {
+.viewFull{
+  display:none;     
+    }
+}
+</style>
+<style>
+body{
+background:black;
+}
+#jembut{
+font-family:Courier;
+color:white;
+position:absolute;
+left:0;
+left:0;
+right:0;
+top:10%;
+}
+#gan{
+font-size:25px;
+font-weight:bold;
+}
+img:hover{
+opacity:1
+}
+hr {
+width:250px;
+}
+a{
+color:white;
+text-decoration:none
+}
+a.hover{
+color:white;
+}
+</style> 
+	<center>
+	  <br> 
+	  <div id="jembut" align="center"> 
+		<div id="gan"> 
+		  <img src="https://scontent-sin6-1.xx.fbcdn.net/v/t1.0-9/79455270_135258504569924_1779422465609433088_n.jpg?_nc_cat=110&amp;_nc_ohc=LsxQs-rmHuIAQlp1ZBPoK5MAbHRh9N7dYZZeO7XMqrq0ju8Q6OPsizl5A&amp;_nc_ht=scontent-sin6-1.xx&amp;oh=a86a7f71a5aba71b3f2703481f4fddaf&amp;oe=5E736198" width="250" height="250" border="0"><br>
+<style type="text/css">
+body { 
+	color: white;
+	font-family:ubuntu_monoregular, monospace; 
+	text-align: center;
+	
+}
+h2,h3,h5{ 
+	font-family: , ; 
+}
+a:link, a:visited {
+	color: gray;
+	text-align: center;
+	text-decoration: none;
+	display: inline-block;}
+a:hover, a:active{
+	color:#4d0000;
+}
+.kedip {
+-webkit-animation-name: blinker;
+-webkit-animation-duration: 1s;
+-webkit-animation-timing-function: linear;
+-webkit-animation-iteration-count: infinite;
+
+-moz-animation-name: blinker;
+-moz-animation-duration: 1s;
+-moz-animation-timing-function: linear;
+-moz-animation-iteration-count: infinite;
+
+animation-name: blinker;
+animation-duration: 1s;
+animation-timing-function: linear;
+animation-iteration-count: infinite;
+
+color: white;}
+
+@-moz-keyframes blinker {0% { opacity: 1.0; }50% { opacity: 0.0; }100% { opacity: 1.0; }}
+@-webkit-keyframes blinker{0% { opacity: 1.0; }50% { opacity: 0.0; }100% { opacity: 1.0; }}
+@keyframes blinker {0% { opacity: 1.0; }50% { opacity: 0.0; }
+100% { opacity: 1.0; }}b {font-size: 16pt;}
+</style> 
+<style>
+body{
+	background:black;
+}
+#och{
+	font-family:Courier;
+	color:white;
+	position:absolute;
+	left:0;
+	left:0;
+	right:0;
+	top:10%;
+}
+#ochh{
+	font-size:25px;
+	font-weight:bold;
+}
+img {
+	opacity:0.5;-webkit-transition:all 250ms ease;-moz-transition:all 250ms ease;-o-transition:all 250ms ease;transition:all 
+
+250ms ease;
+	margin-top:-10px;
+}
+img:hover{	
+	opacity:1
+}
+hr {
+	width:250px;
+}
+a{
+	color:white;
+	text-decoration:none
+}
+a.hover{
+	color:white;
+}
+</style> 
+<style>
+
+#container {
+width: 97vw;
+margin: 2vw auto;
+}
+
+h1 {
+text-align: center;
+color: #fff;
+font-size: 3em;
+letter-spacing: 1px;
+font-family: 'Iceberg';
+font-weight: 400;
+/*Create overlap*/
+
+margin: 0;
+line-height: 0;
+/*Animation*/
+
+-webkit-animation: glitch1 2.5s infinite;
+
+animation: glitch1 2.5s infinite;
+}
+
+h1:nth-child(2) {
+color: #67f3da;
+-webkit-animation: glitch2 2.5s infinite;
+animation: glitch2 2.5s infinite;
+}
+
+h1:nth-child(3) {
+color: #f16f6f;
+-webkit-animation: glitch3 2.5s infinite;
+animation: glitch3 2.5s infinite;
+}
+/*Keyframes*/
+
+@-webkit-keyframes glitch1 {
+0% {
+-webkit-transform: none;
+transform: none;
+opacity: 1;}
+7% {
+-webkit-transform: skew(-0.5deg, -0.9deg);
+transform: skew(-0.5deg, -0.9deg);
+opacity: 0.75;
+}
+10% {
+-webkit-transform: none;
+transform: none;
+opacity: 1;
+}
+27% {
+-webkit-transform: none;
+transform: none;
+opacity: 1;
+}
+30% {
+-webkit-transform: skew(0.8deg, -0.1deg);
+transform: skew(0.8deg, -0.1deg);
+opacity: 0.75;
+}
+35% {
+-webkit-transform: none;
+transform: none;
+opacity: 1;
+}
+52% {
+-webkit-transform: none;
+transform: none;
+opacity: 1;
+}
+55% {
+-webkit-transform: skew(-1deg, 0.2deg);
+transform: skew(-1deg, 0.2deg);
+opacity: 0.75;
+}
+50% {
+-webkit-transform: none;
+transform: none;
+opacity: 1;
+}
+72% {
+-webkit-transform: none;
+transform: none;
+opacity: 1;
+}
+75% {
+-webkit-transform: skew(0.4deg, 1deg);
+transform: skew(0.4deg, 1deg);
+opacity: 0.75;
+}
+80% {
+-webkit-transform: none;
+transform: none;
+opacity: 1;
+}
+100% {
+-webkit-transform: none;
+transform: none;
+opacity: 1;
+}
+}
+
+@keyframes glitch1 {
+0% {
+-webkit-transform: none;
+transform: none;
+opacity: 1;
+}
+}
+
+@keyframes glitch1 {
+0% {
+-webkit-transform: none;
+transform: none;
+opacity: 1;
+}
+7% {
+-webkit-transform: skew(-0.5deg, -0.9deg);
+transform: skew(-0.5deg, -0.9deg);
+opacity: 0.75;
+}
+10% {
+-webkit-transform: none;
+transform: none;
+opacity: 1;
+}
+27% {
+-webkit-transform: none;
+transform: none;
+opacity: 1;
+}
+30% {
+-webkit-transform: skew(0.8deg, -0.1deg);
+transform: skew(0.8deg, -0.1deg);
+opacity: 0.75;
+}
+35% {
+-webkit-transform: none;
+transform: none;
+opacity: 1;
+}
+52% {
+-webkit-transform: none;
+transform: none;
+opacity: 1;
+}
+55% {
+-webkit-transform: skew(-1deg, 0.2deg);
+transform: skew(-1deg, 0.2deg);
+opacity: 0.75;
+}
+50% {
+-webkit-transform: none;
+transform: none;
+opacity: 1;
+}
+72% {
+-webkit-transform: none;
+transform: none;
+opacity: 1;
+}
+75% {
+-webkit-transform: skew(0.4deg, 1deg);
+transform: skew(0.4deg, 1deg);
+opacity: 0.75;
+}
+80% {
+-webkit-transform: none;
+transform: none;
+opacity: 1;
+}
+100% {
+-webkit-transform: none;
+transform: none;
+opacity: 1;
+}
+}
+
+@-webkit-keyframes glitch2 {
+0% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+7% {
+-webkit-transform: translate(-2px, -3px);
+transform: translate(-2px, -3px);
+opacity: 0.5;
+}
+10% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+27% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+30% {
+-webkit-transform: translate(-5px, -2px);
+transform: translate(-5px, -2px);
+opacity: 0.5;
+}
+35% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+52% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+55% {
+-webkit-transform: translate(-5px, -1px);
+transform: translate(-5px, -1px);
+opacity: 0.5;
+}
+50% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+72% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+75% {
+-webkit-transform: translate(-2px, -6px);
+transform: translate(-2px, -6px);
+opacity: 0.5;
+}
+80% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+100% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+}
+
+@keyframes glitch2 {
+0% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+7% {
+-webkit-transform: translate(-2px, -3px);
+transform: translate(-2px, -3px);
+opacity: 0.5;
+}
+10% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+27% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+30% {
+-webkit-transform: translate(-5px, -2px);
+transform: translate(-5px, -2px);
+opacity: 0.5;
+}
+35% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+52% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+55% {
+-webkit-transform: translate(-5px, -1px);
+transform: translate(-5px, -1px);
+opacity: 0.5;
+}
+50% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+72% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+75% {
+-webkit-transform: translate(-2px, -6px);
+transform: translate(-2px, -6px);
+opacity: 0.5;
+}
+80% {
+-webkit-transform: none;
+transform: none;
+}
+100% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+}
+mm
+@-webkit-keyframes glitch3 {
+0% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+7% {
+-webkit-transform: translate(2px, 3px);
+transform: translate(2px, 3px);
+opacity: 0.5;
+}
+10% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+27% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+30% {
+-webkit-transform: translate(5px, 2px);
+transform: translate(5px, 2px);
+opacity: 0.5;
+}
+35% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+52% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+55% {
+-webkit-transform: translate(5px, 1px);
+transform: translate(5px, 1px);
+opacity: 0.5;
+}
+50% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+72% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+75% {
+-webkit-transform: translate(2px, 6px);
+transform: translate(2px, 6px);
+opacity: 0.5;
+}
+80% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+100% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+}
+
+@keyframes glitch3 {
+0% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+7% {
+-webkit-transform: translate(2px, 3px);
+transform: translate(2px, 3px);
+opacity: 0.5;
+10% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+27% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+30% {
+-webkit-transform: translate(5px, 2px);
+transform: translate(5px, 2px);
+opacity: 0.5;
+}
+35% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+52% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+55% {
+-webkit-transform: translate(5px, 1px);
+transform: translate(5px, 1px);
+opacity: 0.5;
+}
+50% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+72% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+75% {
+-webkit-transform: translate(2px, 6px);
+transform: translate(2px, 6px);
+opacity: 0.5;
+}
+80% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+100% {
+-webkit-transform: none;
+transform: none;
+opacity: 0.25;
+}
+}
+body {
+background-color: #000;
+margin-left: 0px; margin-top: 0px; margin-right: 0px; margin-bottom: 0px;}
+body,a:hover{
+cursor:url(http://cur.cursors-4u.net/cursors/cur-10/cur949.png),progress!important;
+}
+</style>
+		  <br><br>
+		  <center>
+			<div id="container"> 
+			  <h1>Hacked By Silent's Space</h1> 
+			  <h1>Hacked By Silent's Space</h1> 
+			  <br>
+		  <br> 
+		  <p> </p>
+		</div>
+		<center>
+		   Want To Be A Best  
+		  <br> Defacer :')
+		  <br><br>
+		  
+		  <br><br>
+		 
+		  
+		  <br> 
+		  <font color="red" size="5">[</font>
+		  <font color="
+		  <br> 
+		  <p> <font color=" grey"="" size="2">Contact?:trustmebrow@gmail.com</font>
+                                             <font color="red" size="5">]</font> <p></p>
+		  <iframe src="https://www.facebook.com/plugins/page.php?href=https://www.facebook.com/hunter404sec/&amp;tabs&amp;width=340&amp;height=154&amp;small_header=true&amp;adapt_container_width=true&amp;hide_cover=false&amp;show_facepile=true&amp;appId" width="340" height="154" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+		  <p> </p>
+		</center>
+	  </center></div>
+	</div></center>
+  
+
+</body></html>
